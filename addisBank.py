84f1d76126f3d3a6b2c2f85e78fbccc26fd6c121
Amanuel <amanuelfekadu37@gmail.com>
2026-07-15 09:49:19 +1400

    start of Addis Bank-AMS project

---
+++ addisBank.py
@@ -0,0 +1,29 @@
+class Account:
+    def __init__(self,owner,account_number,balance):
+        self.owner=owner
+        self.account_number= account_number
+        self.__balance=balance
+
+    @property
+    def balance(self):
+        return self.__balance
+
+    def withdraw(self,amount):
+        if amount > self._balance or amount==0:
+            return "please make sure you have a postive number and suffcient balance"
+        self.__balance-=amount
+        return f"you have withdrawn {amount}ETB"
+
+    def deposite(self,amount):
+        if amount<0:
+            return "please enter a postive number"
+        self.__balance+=amount
+        return f"you have deposited {amount}ETB"
+    def statement(self):
+        return f"Name:{self.owner}\naccount number:{self.account_number}\nbalance:{self.__balance}"
+
+
+Almaze=Account('Almaze',10012,2000)
+print(Almaze.deposite(200))
+        
+        
